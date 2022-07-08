af42d09d0c8c6a68ae3b063878e600249585b8c0
jplima <jplimadev@gmail.com>
2022-07-07 21:43:17 -0300

    insert modal

--- SuperSenha/PasswordsViewController.swift
+++ SuperSenha/PasswordsViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class PasswordsViewController: UIViewController {
-
+    
     @IBOutlet weak var TextViewPassword: UITextView!
     
     var numberOfCharacters: Int = 10
@@ -11,12 +11,27 @@ class PasswordsViewController: UIViewController {
     var useCapitalLetters: Bool!
     var useSpecialCharacters: Bool!
     
+    var passwordGenerator: PasswordGenerator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    @IBAction func generatePassword(_ sender: UIButton) {
+        title = "Total de senhas: \(numberOfPasswords)"
+        passwordGenerator = PasswordGenerator(useNumberOfCharacters: numberOfCharacters, useLetters: useLetters, useNumbers: useNumbers, useCapitalLetters: useCapitalLetters, useSpecialCharacters: useSpecialCharacters)
         
+        generatePassword()
+    }
+    
+    func generatePassword(){
+        TextViewPassword.scrollRangeToVisible(NSRange(location: 0, length: 0))
+        TextViewPassword.text = ""
+        let passwords = passwordGenerator.generate(total: numberOfPasswords)
+        for password in passwords {
+            TextViewPassword.text?.append(password + "\n\n")
+        }
+        
+    }
+    
+    @IBAction func generatePassword(_ sender: UIButton) {
+        generatePassword()
     }
 }
--- SuperSenha/ViewController.swift
+++ SuperSenha/ViewController.swift
@@ -2,9 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var totalPasswords: UITextField!
-    @IBOutlet weak var numberOfCharacters: UITextField!
-    @IBOutlet weak var swletters: UISwitch!
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumberOfCharacters: UITextField!
+    @IBOutlet weak var swLetters: UISwitch!
     @IBOutlet weak var swNumbers: UISwitch!
     @IBOutlet weak var swCapitalLetters: UISwitch!
     @IBOutlet weak var swSpecialCharacters: UISwitch!
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let passwordsViewController = segue.destination as! PasswordsViewController
+        if let numberOfPasswords = Int (tfTotalPasswords.text!) {
+            passwordsViewController.numberOfPasswords = numberOfPasswords
+        }
+        if let numberOfCharacters = Int (tfNumberOfCharacters.text!) {
+            passwordsViewController.numberOfCharacters = numberOfCharacters
+        }
+        passwordsViewController.useLetters = swLetters.isOn
+        passwordsViewController.useNumbers = swNumbers.isOn
+        passwordsViewController.useCapitalLetters = swCapitalLetters.isOn
+        passwordsViewController.useSpecialCharacters = swSpecialCharacters.isOn
+        view.endEditing(true)
+    }
+    
 }
 
